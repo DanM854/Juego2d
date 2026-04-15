a679379b3fb8fa3d902e5716d14e4f7d03d3fc35
Daniel <danielleonardomora.04@gmail.com>
2026-04-14 20:24:36 -0500

    main menu (fix needed)

---
+++ SoulsPlataformer/Assets/_Scripts/ButtonManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+   public void NewGame()
+    {
+        Debug.Log("New Game");
+    }
+
+    public void Continue()
+    {
+        Debug.Log("Continue");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+    }
+}
