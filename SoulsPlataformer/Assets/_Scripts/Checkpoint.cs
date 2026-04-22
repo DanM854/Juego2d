155e1fc272ebaefb0512d6e5e9d4c176792dfc26
Daniel <danielleonardomora.04@gmail.com>
2026-04-21 20:03:03 -0500

    House & fixes

---
+++ SoulsPlataformer/Assets/_Scripts/Checkpoint.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+
+    public bool checkpointActivo = false;
+
+    public Sprite spriteActivo;
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !checkpointActivo)
+        {
+            ActivarCheckpoint();
+        }
+    }
+
+    void ActivarCheckpoint()
+    {
+
+    }
+}
